20a6dad801fd905df520328de2702fe149a5949d
didrbl <st-antonova.e@rggu.ru>
2023-02-03 00:04:34 +0300

    edit js minify

--- js/script.js
+++ js/script.js
@@ -17,12 +17,20 @@
    const burgerItem = document.querySelector('.burger');
    const menu = document.querySelector('.header__nav');
    const menuCloseItem = document.querySelector('.header__nav-close');
+   const menuLinks = document.querySelectorAll('.header__link'); //это не массив, это коллекция ссылок
    burgerItem.addEventListener('click', () => { //обработчик событий по клику
        menu.classList.add('header__nav_active');
    });
    menuCloseItem.addEventListener('click', () => {
        menu.classList.remove('header__nav_active');
    });
+   if (window.innerWidth <= 767) {
+       for (let i = 0; i < menuLinks.length; i += 1) {
+           menuLinks[i].addEventListener('click', () => {
+               menu.classList.remove('header__nav_active');
+           })
+       }
+   }
 }());
 
 // Scroll to anchors
